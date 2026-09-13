e130ca0e4022a5af382f2aff392f003fef1bb077
Asheesh Singh <asheeshsingh0112@gmail.com>
2026-09-13 12:57:21 +0530

    Chapter 8 done

---
+++ Chapter8/Document Structure Based Splitting/python_code_splitting_demo.py
@@ -0,0 +1,56 @@
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter, Language
+
+
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON, chunk_size=300, chunk_overlap=0
+)
+
+text = """
+class Node:
+def __init__(self,endpoint=None):
+self.endpoint = endpoint
+self.child = []
+self.permission = {}
+self.dynamic = None
+class APIGateway:
+def __init__(self):
+self.root = Node("ROOT")
+def load(self,req_method, req_path, perm_id):
+parts = [part for part in req_path.split("/") if part]
+curr = self.root
+for part in parts:
+if part.startswith("{") and part.endswith("}"):
+if not curr.dynamic:
+curr.dynamic = Node(part)
+curr = curr.dynamic
+else:
+temp = None
+for child in curr.child:
+if child.endpoint == part:
+temp = child
+break
+if not temp:
+temp = Node(part)
+curr.child.append(temp)
+curr = temp
+curr.permission[req_method] = perm_id
+def search(self,req_method, req_path):
+parts = [part for part in req_path.split("/") if part]
+curr = self.root
+for part in parts:
+temp = None
+for child in curr.child:
+if child.endpoint == part:
+temp = child
+break
+if not temp:
+temp = curr.dynamic
+if not temp:
+return None
+curr = temp
+return curr.permission[req_method]
+"""
+
+res = splitter.split_text(text=text)
+
+print(res[1])
