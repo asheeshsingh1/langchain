33440d1011d9367a44eccfae28697a7666680890
Asheesh Singh <asheeshsingh0112@gmail.com>
2026-09-15 13:31:41 +0530

    Langfuse demo modified

--- Langsmith And Langfuse/langfuse_demo.py
+++ Langsmith And Langfuse/langfuse_demo.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langfuse import get_client
 from langfuse.langchain import CallbackHandler
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -12,11 +15,29 @@ llm = ChatGoogleGenerativeAI(
     model="gemini-3.5-flash",
 )
 
-response = llm.invoke(
-    "What is the capital of India?",
+
+class Capital(BaseModel):
+    city_name: str
+    language: str = Field(description="Language most of the people speak in that city.")
+
+
+parser = PydanticOutputParser(pydantic_object=Capital)
+
+prompt = PromptTemplate(
+    template="What is the capital city of India? {format}",
+    input_variables=[],
+    partial_variables={"format": parser.get_format_instructions()},
+)
+
+chain = prompt | llm | parser
+
+result = chain.invoke(
+    {},
     config={
         "callbacks": [langfuse_handler],
     },
 )
 
-print(response.content)
+print(result)
+print(result.city_name)
+print(result.language)
